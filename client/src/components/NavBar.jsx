510c2fa4af6b032335d8c3fe0d6af393d259462e
Colten Parker <ColtenParker00@gmail.com>
2025-09-10 14:19:08 -0400

    Fixed Tab Name

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ export default function NavBar() {
     <nav style={{display:'flex',gap:'1rem',padding:'1rem',borderBottom:'1px solid #ddd'}}>
       <Link to="/" style={{fontWeight:700}}>Shonen RPG</Link>
       <NavLink to="/characters">Characters</NavLink>
-      <NavLink to="/teams">Teams</NavLink>
+      <NavLink to="/teams">Team</NavLink>
       <NavLink to="/battle">Battle</NavLink>
     </nav>
   );
